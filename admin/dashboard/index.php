47cfa655a3f1409eb946a4b5457765ca79ad2346
ranvoxrat <markfeliciano36@gmail.com>
2023-05-20 23:00:45 +0800

    landing page

---
+++ admin/dashboard/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once("../../include/initialize.php");
+//checkAdmin();
+  	 if (!isset($_SESSION['USERID'])){
+      redirect(web_root."admin/index.php");
+     }
+
+$view = (isset($_GET['view']) && $_GET['view'] != '') ? $_GET['view'] : '';
+$header=$view;
+$title="Dashboard";
+$content    = 'dashboard.php';		
+
+require_once ("../theme/templates.php");
+?>
+  
